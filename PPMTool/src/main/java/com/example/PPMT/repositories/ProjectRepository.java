7a1866e270f999bcc4a1fe72a4dd737643c944ed
Ekin Küçük <ekin@Ekins-MacBook-Pro.local>
2019-07-17 11:57:25 +0300

    read operation for project

--- PPMTool/src/main/java/com/example/PPMT/repositories/ProjectRepository.java
+++ PPMTool/src/main/java/com/example/PPMT/repositories/ProjectRepository.java
@@ -8,8 +8,7 @@ import com.example.PPMT.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
 
- @Override
- Iterable<Project> findAllById(Iterable<Long> ids);
+Project findByProjectIdentifier(String projectId);
 	
 	
 
