06b5f958ce30de2eec06602d48ee748694c703e0
Ekin Küçük <ekin@Ekins-MacBook-Pro.local>
2019-08-09 14:15:38 +0300

    get all projects final step

--- ppmtool-client/src/components/Dashboard.js
+++ ppmtool-client/src/components/Dashboard.js
@@ -5,11 +5,15 @@ import { Container, Row, Col } from "reactstrap";
 import { connect } from "react-redux";
 import { getProjects } from "../actions/projectActions";
 import PropTypes from "prop-types";
+
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getProjects();
   }
+
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <Container>
@@ -18,9 +22,12 @@ class Dashboard extends Component {
               <h1 className="display-4 text-center">Projects</h1>
               <br />
               <CreateProjectButton />
+
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </Col>
           </Row>
         </Container>
@@ -29,11 +36,15 @@ class Dashboard extends Component {
   }
 }
 
-Dashboard.PropTypes = {
+Dashboard.propTypes = {
   project: PropTypes.object.isRequired,
   getProjects: PropTypes.func.isRequired
 };
-const mapStateToProps = state => ({ project: state.project });
+
+const mapStateToProps = state => ({
+  project: state.project
+});
+
 export default connect(
   mapStateToProps,
   { getProjects }
