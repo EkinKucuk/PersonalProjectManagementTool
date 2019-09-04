9a13635b312a6bdce74b17ea2eb911a11e69b7b7
Ekin Küçük <ekin@Ekins-MacBook-Pro.local>
2019-09-04 15:44:14 +0300

    backlog and project task implementation part 4

---
+++ PPMTool/src/main/java/com/example/PPMT/services/ProjectTaskService.java
@@ -0,0 +1,49 @@
+package com.example.PPMT.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.PPMT.domain.Backlog;
+import com.example.PPMT.domain.ProjectTask;
+import com.example.PPMT.repositories.BacklogRepository;
+import com.example.PPMT.repositories.ProjectTaskRepository;
+
+@Service
+public class ProjectTaskService {
+	
+	@Autowired     
+	private BacklogRepository backlogRepository;
+	
+	@Autowired
+	private ProjectTaskRepository projectTaskRepository;
+
+	
+	public ProjectTask addprojectTask(String projectIdentifier, ProjectTask projectTask) {
+		
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+		
+		projectTask.setBacklog(backlog);
+		
+		Integer backlogSequence = backlog.getPTSequence();
+		backlogSequence++;
+		
+		backlog.setPTSequence(backlogSequence);
+		
+		projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+backlogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
+		
+		
+		if(projectTask.getPriority()==null) {
+			
+			projectTask.setPriority(3);
+		}
+		if(projectTask.getStatus()==""||projectTask.getStatus()==null) {
+			projectTask.setStatus("TO_DO");
+		}
+		
+		
+		
+		return projectTaskRepository.save(projectTask);
+		
+	}
+}
---
+++ PPMTool/src/main/java/com/example/PPMT/web/BacklogController.java
@@ -0,0 +1,48 @@
+package com.example.PPMT.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.PPMT.domain.ProjectTask;
+import com.example.PPMT.services.MapValidationError;
+import com.example.PPMT.services.ProjectTaskService;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+	
+	@Autowired
+	private ProjectTaskService projectTaskService;
+	
+	@Autowired
+	private MapValidationError mapValidationErrorService;
+	
+	@PostMapping("/{backlog_id}")
+	public ResponseEntity<?> addPtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+			BindingResult result, @PathVariable String backlog_id){
+		
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+		
+		if(errorMap!=null) {
+			return errorMap;
+		}
+		
+		
+		ProjectTask projecttask = projectTaskService.addprojectTask(backlog_id, projectTask);
+		
+		return new ResponseEntity<ProjectTask>(projecttask,HttpStatus.CREATED);
+		
+	}
+	
+}
